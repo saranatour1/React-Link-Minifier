199cbcb1f349dac4ee1e632200e5906182ad6c7d
Sara <saranatourcontact@gmail.com>
2023-06-09 17:17:28 +0300

    database connection

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,39 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8000;
+const knex = require("knex");
 
 const bodyParser = require("body-parser"); 
+
+
+require('dotenv').config();
+
+const DATABASE_URL=  process.env.DATABASE_URL; // "239482"
+const PGDATABASE= process.env.PGDATABASE; // "foobar"
+const PGHOST= process.env.PGHOST; // "development"
+const PGPASSWORD =process.env.PGPASSWORD; // "development"
+const PGPORT = process.env.PGPORT;
+const PGUSER = process.env.PGUSER;
+
+console.log(DATABASE_URL);
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: PGHOST,
+    user: PGUSER,
+    password: PGPASSWORD,
+    database: PGDATABASE,
+    ssl: false, // set this to false for it to work 
+  },
+});
+
+db.select("*")
+  .from("urls")
+  .then((data) => {
+    console.log(data);
+  });
+
 
 // app.use(cors());
 app.use(cors({origin: true, credentials: true}));
